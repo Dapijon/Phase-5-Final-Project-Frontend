dac52b895893b561104a9a44dccb472f8807b58e
Hingaz <hhinga04@gmail.com>
2024-02-18 13:51:22 +0300

    Setting up the log in page

--- money-transfer-app/src/Components/Home.js
+++ money-transfer-app/src/Components/Home.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-       <div className='header'>
-        <img className='image'src='https://cdn-icons-png.freepik.com/256/10383/10383229.png'></img>
-        <h1 className='home-header'>PINACLE FINANCE </h1>
-    </div>
-    <div className='body'>
-       <h2> The future of MOBILE BANKING</h2>
-       <img src='https://www.shutterstock.com/image-photo/mobile-shopping-cheerful-arabic-woman-600nw-2108487698.jpg'></img>
-    </div>
+      <div className="header">
+        <img
+          className="image"
+          src="https://cdn-icons-png.freepik.com/256/10383/10383229.png"
+        ></img>
+        <h1 className="home-header">PINACLE FINANCE </h1>
+      </div>
+      <div className="body">
+        <div className="col-1">
+          <h2> THE FUTURE OF MOBILE BANKING</h2>
+          <img
+            className="phone-image"
+            src="https://www.shutterstock.com/image-photo/mobile-shopping-cheerful-arabic-woman-600nw-2108487698.jpg"
+          ></img>
+        </div>
+        <div className="col-2">
+          <p> Experience the best in mobile banking</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
