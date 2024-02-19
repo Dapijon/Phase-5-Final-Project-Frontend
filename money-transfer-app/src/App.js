bbf466f27dba55beffb60181f6795d317b32fdc8
leonkinyanjui <dapijon@gmail.com>
2024-02-19 23:29:48 +0300

    Intergration of transactions page with App.js

--- money-transfer-app/src/App.js
+++ money-transfer-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import TransactionPage from './Components/TransactionsPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home/>}></Route>
           <Route path="/signup" element={<SignUp/>}></Route>
           <Route path="/login" element={<Login/>}></Route>
+          <Route path='/transactions' element={<TransactionPage />} />
         </Routes>
       </Router>
     </div>
--- money-transfer-app/src/Components/TransactionsPage.js
+++ money-transfer-app/src/Components/TransactionsPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./TransactionsPage.css"
 
 const TransactionPage = () => {
   const [depositAmount, setDepositAmount] = useState('');
