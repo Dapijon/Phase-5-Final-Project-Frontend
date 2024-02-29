714a2364d66a5971a722a56e078111b8a78975c1
Walter N <2760walter@gmail.com>
2024-02-29 09:18:50 +0300

    render nav

--- money-transfer-app/src/Components/Navbar.js
+++ money-transfer-app/src/Components/Navbar.js
@@ -1,40 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-//import { useAuth, logout } from "../auth";
+import { useSelector, useDispatch } from 'react-redux';
+import { signOut } from '../app/authSlice';
 
 function Navbar() {
-  //const [logged] = useAuth();
-  return (
+  const accessToken = useSelector((state) => state.auth.accessToken); 
 
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    
+    dispatch(signOut());
+  };
+
+  return (
     <div className="nav-items">
       <div className="header-icon">
         <img
           className="nav-img"
           src="https://cdn-icons-png.freepik.com/256/10383/10383229.png"
-        ></img>
-
+          alt="logo"
+        />
         <div className="nav-header">
-          <h1 className="home-heading">PINACLE FINANCE </h1>
-          <p> Experience the best in mobile banking</p>
+          <h1 className="home-heading">PINACLE FINANCE</h1>
+          <p>Experience the best in mobile banking</p>
         </div>
-
       </div>
-      <nav className="navbar ">
-
+      <nav className="navbar">
         <ul className="navbar-links">
-          <li className="nav-item">
-            <Link to="/signup">Sign Up</Link>
+          {accessToken ? (
+            <>
+              <li className="nav-item">
+                <Link to="/analytics">Transactions</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/admin">Users</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/send">Send</Link>
+              </li>
+              <li className="nav-item">
+            <button onClick={handleLogout}>Logout</button>
           </li>
-          <li className="nav-item">
-            <Link to="/login">Login</Link>
-          </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-item">
+                <Link to="/login">Login</Link>
+                </li>
+                <li className="nav-item nav-item-btn">
+                <Link to="/signup">SignUp</Link>
+              </li>
+             
+            </>
+          )}
         </ul>
       </nav>
     </div>
   );
 }
-
-
 
 export default Navbar;
